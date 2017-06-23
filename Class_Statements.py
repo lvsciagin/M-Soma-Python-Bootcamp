58faff4c7ffd8f28a435716a65147b1143d825ce
lvsciagin <lvsciagin@gmail.com>
2017-06-23 10:06:38 +0300

    Class statements

---
+++ Class_Statements.py
@@ -0,0 +1,11 @@
+# Class Methods
+
+from math import sqrt
+
+class Clown:
+	nose = 'big and red'
+	bag = 'Black and Yellow'
+	def dance():
+		return 'No Thanks'
+	def summation(x):
+		return sqrt(x)
