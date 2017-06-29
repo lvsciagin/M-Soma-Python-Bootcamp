6230c2ded2fa716f9584f25a52ee4300900abcb0
lvsciagin <lvsciagin@gmail.com>
2017-06-29 11:18:17 +0300

    Machine learning Tweet fetch

--- Class_Statements.py
+++ Class_Statements.py
@@ -9,3 +9,17 @@ class Clown:
 		return 'No Thanks'
 	def summation(x):
 		return sqrt(x)
+
+class Account:
+	def __init__(self, account_holder):
+		self.holder = account_holder
+		self.balance = 0
+	def deposit(self, amount):
+		self.balance += amount
+		return self.balance
+	def withdraw (self, amount):
+		if self.balance > amount:
+			self.balance-= amount
+			return self.balance
+		else:
+			print('Insufficient balance')
